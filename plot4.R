03aa8bf5cc11bbc1abc86e5c71f8255ba5ef4bc1
David Chen <cswdc@icloud.com>
2016-02-07 13:33:29 +0800

    reuploading the correct ones

--- plot4.R
+++ plot4.R
@@ -5,6 +5,7 @@ neededData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 dateTime <- strptime(paste(neededData$Date, neededData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
 globalActivePower <- as.numeric(neededData$Global_active_power) 
+globalReactivePower <- as.numeric(neededData$Global_reactive_power)
 voltage <- as.numeric(neededData$Voltage)
 subMetering1 <- as.numeric(neededData$Sub_metering_1) 
 subMetering2 <- as.numeric(neededData$Sub_metering_2) 
@@ -27,7 +28,7 @@ lines(dateTime, subMetering3, col = "blue")
 legend("topright", bty = "n", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 1) 
 
 # Plot 4
-plot(dateTime, globalActivePower, type = "l", col = "black", xlab = "datetime", ylab = colnames(neededData)[4]) 
+plot(dateTime, globalReactivePower, type = "l", col = "black", xlab = "datetime", ylab = "Global_reactive_power") 
 
 dev.off() 
 
